0ce52f6458e135f8d33af89efeca944625c64cc9
Victus <asadbekraimjonov09@gmail.com>
2026-09-20 21:17:33 +0500

    modified

--- src/views/main/Contact.vue
+++ src/views/main/Contact.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="absolute inset-0 z-0">
         <img class="absolute inset-0 h-full w-full object-cover" src="@/assets/images/truck_footer.png" alt="">
-        <div class="absolute inset-0 bg-black/80"></div>
+        <div class="absolute inset-0 bg-black/70"></div>
 
         <div class="relative z-10 text-white px-32 py-16 flex items-center">
             <MapAmerica class="w-[60%]" />
